e903f12c21cd09229a735e8fe0f7e01148c077b0
atacomp84 <95216550+atacomp84@users.noreply.github.com>
2025-12-02 08:10:51 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.syncmind',
+  appName: 'SyncMind',
+  webDir: 'dist'
+};
+
+export default config;
